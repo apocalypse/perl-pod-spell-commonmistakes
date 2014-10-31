0affe595891dad561aa72d48e9f1c82d4368b249
Apocalypse <APOCAL@cpan.org>
2014-10-31 10:54:26 -0700

    first stab at adaptor for Pod::Spelling

---
+++ lib/Pod/Spelling/CommonMistakes.pm
@@ -0,0 +1,30 @@
+package Pod::Spelling::CommonMistakes;
+
+# ABSTRACT: Adaptor for Pod::Spelling to use CommonMistakes as a checker
+
+use base 'Pod::Spelling'; # Thanks :)
+
+use Pod::Spell::CommonMistakes::WordList qw( _check_common _check_case );
+
+# We override our base...
+no warnings 'redefine';
+
+sub _init {
+	shift if not ref $_[0];
+	my $self = shift;
+
+	# Nothing magical to do :)
+
+	return $self;
+}
+
+# Accepts one or more lines of text, returns a list mispelt words.
+sub _spell_check_callback {
+        my $self = shift;
+        my @lines = @_;
+        @lines = grep { not $_ } split /\s+/, join( ' ', @lines );
+	my $err = _check_common( \@lines );
+	$err = { %$err, %{ _check_case( \@lines ) } };
+
+        return keys %$err;
+}
